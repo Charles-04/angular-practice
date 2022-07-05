7d3b8664bd5b3aeaae7be5c89183e287160b6778
Charles <charlesmir04@gmail.com>
2022-07-05 15:17:53 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myApp';
+  title:string = 'myApp';
+  descrip:string = "Angular class by Mike";
+  isClassEnded:boolean = false;
+  numberOfStudents:number = 7;
+  nameOfstudent:string='Charles';
+  studentNeed:string='food';
+  userName:string='Charlie too good';
+  showMenu:boolean = false;
+  dishes:string[]=[
+    "Beans and plantain",
+    "Draw soup",
+    "Abacha",
+    "Bread and coconut",
+    "Fio Fio and yam",
+    "Noodles and egg",
+    "fried plantain and potato",
+    "Okpa and coke",
+    "Kpoff Kpoff"
+  ]
+  
+  
+  toggleMenu():void{
+    this.showMenu = !this.showMenu
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './pages/todos/todos.component';
+import { TodoComponent } from './pages/todo/todo.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TodosComponent, TodoComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
